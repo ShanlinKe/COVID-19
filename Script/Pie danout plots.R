f1e39766d71455546a9aadcdde88062e3eb9bd78
Owenke247 <83370369+Owenke247@users.noreply.github.com>
2022-04-05 14:16:08 -0400

    Update Pie danout plots.R

--- Script/Pie danout plots.R
+++ Script/Pie danout plots.R
@@ -6,7 +6,7 @@ library(cowplot)
 library(RColorBrewer)
 library(webr)
 library(dplyr)
-setwd("/Users/shanlin/HMS_shanlin/Project/COVID19/Analysis/Basics")
+setwd("/path to data/Basics")
 df1 <- read.table("COVID_overview.txt",header=T)
 
 p01 <- PieDonut(df1, aes(Label, Cohort, count=Sample), title = "COVID19 datasets",donutLabelSize = 3, r1=0.8)
@@ -18,4 +18,4 @@ gg1 <- ggMarginal(p1, type = "density", fill = "#2c7fb8", alpha = .2)
 
 All1 <- plot_grid(gg1,labels=c('a'),label_size = 10,ncol=1)
 
-save_plot("COVID19_Fig2a-c.pdf", All1, base_width = 9,base_height =8)
+save_plot("COVID19_dis.pdf", All1, base_width = 9,base_height =8)
