671131531017558e0a0976cd44ddf233a0d9d9a6
Owenke247 <83370369+Owenke247@users.noreply.github.com>
2022-04-05 13:32:40 -0400

    Update 02_assembly.sh

--- Pipeline/02_assembly.sh
+++ Pipeline/02_assembly.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+#$ -l vf=160G
+#$ -cwd
+#$ -N cov-assm11
 
+metawrap assembly -1 /path to clean data/XXX_1.fastq -2 /path to clean data/XXX_2.fastq -m 200 -t 20 --metaspades -o /path to save data/XXX
