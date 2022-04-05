95b997e7313858d9642f0b2832d5cd3c57d127c9
Owenke247 <83370369+Owenke247@users.noreply.github.com>
2022-04-05 13:30:51 -0400

    Update 01_quality_control.sh

--- Pipeline/01_quality_control.sh
+++ Pipeline/01_quality_control.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+#$ -l vf=100G
+#$ -cwd
+#$ -N QC_covid1
 
+metawrap read_qc -1 /path to sequencing data/XXX_1.fastq -2 /path to sequencing data/XXX_2.fastq -t 10 -o /path of saving data/XXX
