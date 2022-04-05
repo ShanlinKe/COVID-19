28e38c2877d70ec3f7667bbd0da590f8e2ba26b1
Owenke247 <83370369+Owenke247@users.noreply.github.com>
2022-04-05 14:12:32 -0400

    Update and rename Genome reconstruction to Pie danout plots.R

---
+++ Script/Pie danout plots.R
@@ -0,0 +1,21 @@
+
+################### Fig 2A-C Pie danout plots ##############
+rm(list=ls())
+library(ggplot2)
+library(cowplot)
+library(RColorBrewer)
+library(webr)
+library(dplyr)
+setwd("/Users/shanlin/HMS_shanlin/Project/COVID19/Analysis/Basics")
+df1 <- read.table("COVID_overview.txt",header=T)
+
+p01 <- PieDonut(df1, aes(Label, Cohort, count=Sample), title = "COVID19 datasets",donutLabelSize = 3, r1=0.8)
+p02 <- PieDonut(df1, aes(Label, Cohort, count=MAG), title = "MAG distribution",donutLabelSize = 3, r1=0.8)
+p03 <- PieDonut(df1, aes(Label, Cohort, count=NR_MAG), title = "NR_MAG distribution",donutLabelSize = 3, r1=0.8)
+
+
+gg1 <- ggMarginal(p1, type = "density", fill = "#2c7fb8", alpha = .2)
+
+All1 <- plot_grid(gg1,labels=c('a'),label_size = 10,ncol=1)
+
+save_plot("COVID19_Fig2a-c.pdf", All1, base_width = 9,base_height =8)
