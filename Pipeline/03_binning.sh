a890f13ca76a50f112ef9c763101e9152531157c
Owenke247 <83370369+Owenke247@users.noreply.github.com>
2022-04-05 13:34:41 -0400

    Update 03_binning.sh

--- Pipeline/03_binning.sh
+++ Pipeline/03_binning.sh
@@ -1,1 +1,7 @@
 
+#!/bin/bash
+#$ -l vf=120G
+#$ -cwd
+#$ -N cvd_bin1
+
+metawrap binning -o /path to save Bins/XXX -t 10 -a /path to assembled contigs/final_assembly.fasta --metabat2 --maxbin2 --concoct /path to clean data/XXX*.fastq
